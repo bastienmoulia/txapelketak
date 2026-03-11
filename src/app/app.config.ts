7d755f3c2bc80289a1d118ff421d5025c216e284
Bastien <bastien.moulia@sdworx.com>
2026-03-11 13:35:41 +0100

    feat: integrate Firebase with Angular application
    - Added @angular/fire dependency to package.json for Firebase integration.
    - Configured Firebase app initialization and Firestore provision in app.config.ts.
    - Created .firebaserc for Firebase project configuration.
    - Added GitHub Actions workflows for Firebase Hosting deployment on merge and pull request.
    - Included .npmrc to handle legacy peer dependencies.
    - Created firebase.json for Firebase Hosting configuration.
    - Added index.html for Firebase Hosting setup with SDK initialization.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,8 @@ import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { providePrimeNG } from "primeng/config";
 import Aura from "@primeuix/themes/aura";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +19,18 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
       },
     }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: "txapelketak-ac529",
+        appId: "1:210687482712:web:22ae72552902c05891e32d",
+        storageBucket: "txapelketak-ac529.firebasestorage.app",
+        apiKey: "AIzaSyAml_FZRsRZjrClzGdbqyWkWTEQ3SKmTlA",
+        authDomain: "txapelketak-ac529.firebaseapp.com",
+        messagingSenderId: "210687482712",
+        projectNumber: "210687482712",
+        version: "2",
+      }),
+    ),
+    provideFirestore(() => getFirestore()),
   ],
 };

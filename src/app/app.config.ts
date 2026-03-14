884774153d1726285748ea1ef7dc68b5973dbe5d
Bastien <bmoulia@gmail.com>
2026-03-14 09:37:40 +0100

    feat: configure Firebase emulators and update environment settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,12 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-} from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import { routes } from "./app.routes";
-import { providePrimeNG } from "primeng/config";
-import Aura from "@primeuix/themes/aura";
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectFirestoreEmulator, getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,14 +19,24 @@ export const appConfig: ApplicationConfig = {
     }),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: "txapelketak-ac529",
-        appId: "1:210687482712:web:22ae72552902c05891e32d",
-        storageBucket: "txapelketak-ac529.firebasestorage.app",
-        apiKey: "AIzaSyAml_FZRsRZjrClzGdbqyWkWTEQ3SKmTlA",
-        authDomain: "txapelketak-ac529.firebaseapp.com",
-        messagingSenderId: "210687482712",
+        projectId: 'txapelketak-ac529',
+        appId: '1:210687482712:web:22ae72552902c05891e32d',
+        storageBucket: 'txapelketak-ac529.firebasestorage.app',
+        apiKey: 'AIzaSyAml_FZRsRZjrClzGdbqyWkWTEQ3SKmTlA',
+        authDomain: 'txapelketak-ac529.firebaseapp.com',
+        messagingSenderId: '210687482712',
       }),
     ),
-    provideFirestore(() => getFirestore()),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+
+      if (environment.useFirestoreEmulator) {
+        try {
+          connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+        } catch {}
+      }
+
+      return firestore;
+    }),
   ],
 };

1ab373de6020eca92c1345415d746240788a22ec
Bastien <bmoulia@gmail.com>
2026-03-19 22:50:14 +0100

    feat: enhance Games component tests with Transloco testing providers

--- src/app/tournaments/types/shared/games/games.spec.ts
+++ src/app/tournaments/types/shared/games/games.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Games } from './games';
+import { provideTranslocoTesting } from '../../../../testing/transloco-testing.providers';
 
 describe('Games', () => {
   let component: Games;
@@ -9,9 +10,13 @@ describe('Games', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Games],
+      providers: [...provideTranslocoTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Games);
+    fixture.componentRef.setInput('teams', []);
+    fixture.componentRef.setInput('series', []);
+    fixture.detectChanges();
     component = fixture.componentInstance;
     await fixture.whenStable();
   });

a0c066f06e90bdbf6628a3cedc205d4097242790
Bastien <bmoulia@gmail.com>
2026-04-11 18:15:42 +0200

    feat: add Transloco testing provider to app component tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { App } from './app';
+import { provideTranslocoTesting } from './testing/transloco-testing.providers';
+import { providePrimeNG } from 'primeng/config';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideRouter([])],
+      providers: [provideRouter([]), provideTranslocoTesting(), providePrimeNG({})],
     }).compileComponents();
   });
 
